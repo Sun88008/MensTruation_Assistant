c4a552c212530c9b87f432ed25a4ed354853ccc3
DoubleS <812009938@qq.com>
2019-03-13 11:13:02 +0800

    登录界面优化
    新增闭眼动画

--- Podfile
+++ Podfile
@@ -4,6 +4,7 @@ target 'Yima3’ do
     pod 'LeanCloud'
     pod 'AVOSCloud'             # 数据存储、短信、云引擎调用等基础服务模块
     pod 'AVOSCloudIM'           # 即时通讯模块
+    pod 'Alamofire', '~> 4.2.0’
 #pod 'SnapKit', '~> 3.2.0'
 xcodeproj ‘Yima3.xcodeproj'
 end
--- Yima3/SignViewController.swift
+++ Yima3/SignViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import LeanCloud
 import AVOSCloud
 import SnapKit
+import Alamofire
 
 class SignViewController: UIViewController, UITextFieldDelegate {
     
@@ -19,9 +20,10 @@ class SignViewController: UIViewController, UITextFieldDelegate {
     var horizontalLine: UIView! //分隔线
     var confirmButton:UIButton! //登录按钮
     var titleLabel: UILabel! //标题标签
+    var imgLogin:UIImageView!
     
     var topConstraint: Constraint? //登录框距顶部距离约束
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,7 +31,7 @@ class SignViewController: UIViewController, UITextFieldDelegate {
         self.view.backgroundColor = UIColor(red: 255/255, green: 192/255, blue: 203/255,
                                             alpha: 1)
         
-        //登录框高度
+        /*//登录框高度
         let formViewHeight = 90
         //登录框背景
         self.formView = UIView()
@@ -64,19 +66,19 @@ class SignViewController: UIViewController, UITextFieldDelegate {
         
         //密码图标
         let imgLock2 =  UIImageView(frame:CGRect(x: 11, y: 11, width: 22, height: 22))
-        imgLock2.image = UIImage(named:"mima")
+        imgLock2.image = UIImage(named:"mima")*/
         
         //用户名输入框
-        self.txtUser = UITextField()
+        /*self.txtUser = UITextField()
         self.txtUser.delegate = self
         self.txtUser.placeholder = "用户名"
         self.txtUser.tag = 100
         self.txtUser.leftView = UIView(frame:CGRect(x: 0, y: 0, width: 44, height: 44))
         self.txtUser.leftViewMode = UITextFieldViewMode.always
-        self.txtUser.returnKeyType = UIReturnKeyType.next
+        self.txtUser.returnKeyType = UIReturnKeyType.next*/
         
         //用户名输入框左侧图标
-        self.txtUser.leftView!.addSubview(imgLock1)
+        /*self.txtUser.leftView!.addSubview(imgLock1)
         self.formView.addSubview(self.txtUser)
         
         //布局
@@ -122,11 +124,11 @@ class SignViewController: UIViewController, UITextFieldDelegate {
             make.top.equalTo(self.formView.snp.bottom).offset(20)
             make.right.equalTo(-15)
             make.height.equalTo(44)
-        }
+        }*/
         
-        //标题label
+        /*//标题label
         self.titleLabel = UILabel()
-        self.titleLabel.text = "姨妈助手"
+        self.titleLabel.text = ""//"姨妈助手"
         self.titleLabel.textColor = UIColor.white
         self.titleLabel.font = UIFont.systemFont(ofSize: 36)
         self.view.addSubview(self.titleLabel)
@@ -134,7 +136,101 @@ class SignViewController: UIViewController, UITextFieldDelegate {
             make.bottom.equalTo(self.formView.snp.top).offset(-20)
             make.centerX.equalTo(self.view)
             make.height.equalTo(44)
+        }*/
+        
+        //获取屏幕尺寸
+        let mainSize = UIScreen.main.bounds.size
+        
+        //猫头鹰头部
+        self.imgLogin = UIImageView(frame: CGRect(x: mainSize.width/2-211/2, y: 100, width: 211, height: 109))
+            //UIImageView(frame:CGRectMake(, 100, 211, 109))
+        imgLogin.image = UIImage(named:"owl-login")
+        imgLogin.layer.masksToBounds = true
+        self.view.addSubview(imgLogin)
+        
+        
+        //猫头鹰左手(遮眼睛的)
+        let rectLeftHand = CGRect(x:61 - offsetLeftHand, y:90, width:40, height:65)
+        imgLeftHand = UIImageView(frame:rectLeftHand)
+        imgLeftHand.image = UIImage(named:"owl-login-arm-left")
+        imgLogin.addSubview(imgLeftHand)
+        
+        //猫头鹰右手(遮眼睛的)
+        let rectRightHand = CGRect(x:imgLogin.frame.size.width / 2 + 60, y:90, width:40, height:65)
+        imgRightHand = UIImageView(frame:rectRightHand)
+        imgRightHand.image = UIImage(named:"owl-login-arm-right")
+        imgLogin.addSubview(imgRightHand)
+        
+        //登录框背景
+        let vLogin =  UIView(frame:CGRect(x:15, y:200, width:mainSize.width - 30, height:160))
+        vLogin.layer.borderWidth = 0.5
+        vLogin.layer.cornerRadius = 10 //四周角的尖锐度
+        vLogin.layer.borderColor = UIColor.lightGray.cgColor
+        vLogin.backgroundColor = UIColor.white
+        self.view.addSubview(vLogin)
+        
+        //登录按钮
+        self.confirmButton = UIButton()
+        self.confirmButton.setTitle("登录", for: UIControlState())
+        self.confirmButton.setTitleColor(UIColor.black,
+                                         for: UIControlState())
+        self.confirmButton.layer.cornerRadius = 5
+        self.confirmButton.backgroundColor = UIColor(red: 1, green: 1, blue: 1,alpha: 0.5)
+        self.confirmButton.addTarget(self, action: #selector(loginConfrim),for: .touchUpInside)
+        self.view.addSubview(self.confirmButton)
+        self.confirmButton.snp.makeConstraints { (make) -> Void in
+            make.left.equalTo(15)
+            make.top.equalTo(vLogin.snp.bottom).offset(20)
+            make.right.equalTo(-15)
+            make.height.equalTo(44)
         }
+        
+        //用户名框
+        txtUser = UITextField(frame:CGRect(x:30, y:30, width:vLogin.frame.size.width - 60, height:44))
+        txtUser.delegate = self
+        txtUser.layer.cornerRadius = 5
+        txtUser.layer.borderColor = UIColor.lightGray.cgColor
+        txtUser.layer.borderWidth = 0.5
+        txtUser.leftView = UIView(frame:CGRect(x:0, y:0, width:44, height:44))
+        txtUser.leftViewMode = UITextFieldViewMode.always
+        
+        let imgUser =  UIImageView(frame:CGRect(x:11, y:11, width:22, height:22))
+        imgUser.image = UIImage(named:"icon_zhanghao")
+        txtUser.leftView!.addSubview(imgUser)
+        vLogin.addSubview(txtUser)
+        
+        //密码输入框
+        txtPwd = UITextField(frame:CGRect(x:30, y:90, width:vLogin.frame.size.width - 60, height:44))
+        txtPwd.delegate = self
+        txtPwd.layer.cornerRadius = 5
+        txtPwd.layer.borderColor = UIColor.lightGray.cgColor
+        txtPwd.layer.borderWidth = 0.5
+        //txtPwd.secureTextEntry = true
+        txtPwd.leftView = UIView(frame:CGRect(x:0, y:0, width:44, height:44))
+        txtPwd.leftViewMode = UITextFieldViewMode.always
+        
+        //密码输入框左侧图标
+        let imgPwd =  UIImageView(frame:CGRect(x:11, y:11, width:22, height:22))
+        imgPwd.image = UIImage(named:"mima")
+        txtPwd.leftView!.addSubview(imgPwd)
+        vLogin.addSubview(txtPwd)
+        
+        
+        //猫头鹰左手(圆形的)
+        let rectLeftHandGone = CGRect(x:mainSize.width / 2 - 100,
+                                      y:vLogin.frame.origin.y - 22, width:40, height:40)
+        imgLeftHandGone = UIImageView(frame:rectLeftHandGone)
+        imgLeftHandGone.image = UIImage(named:"icon_hand")
+        self.view.addSubview(imgLeftHandGone)
+        
+        //猫头鹰右手(圆形的)
+        let rectRightHandGone = CGRect(x:mainSize.width / 2 + 62,
+                                       y:vLogin.frame.origin.y - 22, width:40, height:40)
+        imgRightHandGone = UIImageView(frame:rectRightHandGone)
+        imgRightHandGone.image = UIImage(named:"icon_hand")
+        self.view.addSubview(imgRightHandGone)
+        
+        
     }
     
     //输入框获取焦点开始编辑
@@ -144,6 +240,62 @@ class SignViewController: UIViewController, UITextFieldDelegate {
             self.topConstraint?.update(offset: -125)
             self.view.layoutIfNeeded()
         })
+        
+        //如果当前是用户名输入
+        if textField.isEqual(txtUser){
+            if (showType != LoginShowType.PASS)
+            {
+                showType = LoginShowType.USER
+                return
+            }
+            showType = LoginShowType.USER
+            
+            //播放不遮眼动画
+            UIView.animate(withDuration: 0.5, animations: { () -> Void in
+                self.imgLeftHand.frame = CGRect(
+                    x:self.imgLeftHand.frame.origin.x - self.offsetLeftHand,
+                    y:self.imgLeftHand.frame.origin.y + 30,
+                    width:self.imgLeftHand.frame.size.width, height:self.imgLeftHand.frame.size.height)
+                self.imgRightHand.frame = CGRect(
+                    x:self.imgRightHand.frame.origin.x + 48,
+                    y:self.imgRightHand.frame.origin.y + 30,
+                    width:self.imgRightHand.frame.size.width, height:self.imgRightHand.frame.size.height)
+                self.imgLeftHandGone.frame = CGRect(
+                    x:self.imgLeftHandGone.frame.origin.x - 70,
+                    y:self.imgLeftHandGone.frame.origin.y, width:40, height:40)
+                self.imgRightHandGone.frame = CGRect(
+                    x:self.imgRightHandGone.frame.origin.x + 30,
+                    y:self.imgRightHandGone.frame.origin.y, width:40, height:40)
+            })
+        }
+            //如果当前是密码名输入
+        else if textField.isEqual(txtPwd){
+            if (showType == LoginShowType.PASS)
+            {
+                showType = LoginShowType.PASS
+                return
+            }
+            showType = LoginShowType.PASS
+            
+            //播放遮眼动画
+            UIView.animate(withDuration: 0.5, animations: { () -> Void in
+                self.imgLeftHand.frame = CGRect(
+                    x:self.imgLeftHand.frame.origin.x + self.offsetLeftHand,
+                    y:self.imgLeftHand.frame.origin.y - 30,
+                    width:self.imgLeftHand.frame.size.width, height:self.imgLeftHand.frame.size.height)
+                self.imgRightHand.frame = CGRect(
+                    x:self.imgRightHand.frame.origin.x - 48,
+                    y:self.imgRightHand.frame.origin.y - 30,
+                    width:self.imgRightHand.frame.size.width, height:self.imgRightHand.frame.size.height)
+                self.imgLeftHandGone.frame = CGRect(
+                    x:self.imgLeftHandGone.frame.origin.x + 70,
+                    y:self.imgLeftHandGone.frame.origin.y, width:0, height:0)
+                self.imgRightHandGone.frame = CGRect(
+                    x:self.imgRightHandGone.frame.origin.x - 30,
+                    y:self.imgRightHandGone.frame.origin.y, width:0, height:0)
+            })
+        }
+        
     }
     
     //输入框返回时操作
@@ -172,8 +324,193 @@ class SignViewController: UIViewController, UITextFieldDelegate {
         })
         //self.present(TabBarController!, animated: true, completion: nil)
     }
+    
+    //左手离脑袋的距离
+    var offsetLeftHand:CGFloat = 60
+    
+    //左手图片,右手图片(遮眼睛的)
+    var imgLeftHand:UIImageView!
+    var imgRightHand:UIImageView!
+    
+    //左手图片,右手图片(圆形的)
+    var imgLeftHandGone:UIImageView!
+    var imgRightHandGone:UIImageView!
+    
+    //登录框状态
+    var showType:LoginShowType = LoginShowType.NONE
+    
+    enum LoginShowType {
+        case NONE
+        case USER
+        case PASS
+    }
 }
 
+/*//用户密码输入框
+ 
+ //左手离脑袋的距离
+ var offsetLeftHand:CGFloat = 60
+ 
+ //左手图片,右手图片(遮眼睛的)
+ var imgLeftHand:UIImageView!
+ var imgRightHand:UIImageView!
+ 
+ //左手图片,右手图片(圆形的)
+ var imgLeftHandGone:UIImageView!
+ var imgRightHandGone:UIImageView!
+ 
+ //登录框状态
+ var showType:LoginShowType = LoginShowType.NONE
+ 
+ override func viewDidLoad() {
+ super.viewDidLoad()
+ 
+ //获取屏幕尺寸
+ let mainSize = UIScreen.main.bounds.size
+ 
+ //猫头鹰头部
+ self.imgLogin = UIImageView(frame: CGRect(x: mainSize.width/2-211/2, y: 100, width: 211, height: 109))
+ //UIImageView(frame:CGRectMake(, 100, 211, 109))
+ imgLogin.image = UIImage(named:"owl-login")
+ imgLogin.layer.masksToBounds = true
+ self.view.addSubview(imgLogin)
+ 
+ 
+ //猫头鹰左手(遮眼睛的)
+ let rectLeftHand = CGRect(x:61 - offsetLeftHand, y:90, width:40, height:65)
+ imgLeftHand = UIImageView(frame:rectLeftHand)
+ imgLeftHand.image = UIImage(named:"owl-login-arm-left")
+ imgLogin.addSubview(imgLeftHand)
+ 
+ //猫头鹰右手(遮眼睛的)
+ let rectRightHand = CGRect(x:imgLogin.frame.size.width / 2 + 60, y:90, width:40, height:65)
+ imgRightHand = UIImageView(frame:rectRightHand)
+ imgRightHand.image = UIImage(named:"owl-login-arm-right")
+ imgLogin.addSubview(imgRightHand)
+ 
+ //登录框背景
+ let vLogin =  UIView(frame:CGRect(x:15, y:200, width:mainSize.width - 30, height:160))
+ vLogin.layer.borderWidth = 0.5
+ vLogin.layer.borderColor = UIColor.lightGray.cgColor
+ vLogin.backgroundColor = UIColor.white
+ self.view.addSubview(vLogin)
+ 
+ //用户名框
+ txtUser = UITextField(frame:CGRect(x:30, y:30, width:vLogin.frame.size.width - 60, height:44))
+ txtUser.delegate = self
+ txtUser.layer.cornerRadius = 5
+ txtUser.layer.borderColor = UIColor.lightGray.cgColor
+ txtUser.layer.borderWidth = 0.5
+ txtUser.leftView = UIView(frame:CGRect(x:0, y:0, width:44, height:44))
+ txtUser.leftViewMode = UITextFieldViewMode.always
+ 
+ let imgUser =  UIImageView(frame:CGRect(x:11, y:11, width:22, height:22))
+ imgUser.image = UIImage(named:"icon_zhanghao")
+ txtUser.leftView!.addSubview(imgUser)
+ vLogin.addSubview(txtUser)
+ 
+ //密码输入框
+ txtPwd = UITextField(frame:CGRect(x:30, y:90, width:vLogin.frame.size.width - 60, height:44))
+ txtPwd.delegate = self
+ txtPwd.layer.cornerRadius = 5
+ txtPwd.layer.borderColor = UIColor.lightGray.cgColor
+ txtPwd.layer.borderWidth = 0.5
+ //txtPwd.secureTextEntry = true
+ txtPwd.leftView = UIView(frame:CGRect(x:0, y:0, width:44, height:44))
+ txtPwd.leftViewMode = UITextFieldViewMode.always
+ 
+ //密码输入框左侧图标
+ let imgPwd =  UIImageView(frame:CGRect(x:11, y:11, width:22, height:22))
+ imgPwd.image = UIImage(named:"mima")
+ txtPwd.leftView!.addSubview(imgPwd)
+ vLogin.addSubview(txtPwd)
+ 
+ 
+ //猫头鹰左手(圆形的)
+ let rectLeftHandGone = CGRect(x:mainSize.width / 2 - 100,
+ y:vLogin.frame.origin.y - 22, width:40, height:40)
+ imgLeftHandGone = UIImageView(frame:rectLeftHandGone)
+ imgLeftHandGone.image = UIImage(named:"icon_hand")
+ self.view.addSubview(imgLeftHandGone)
+ 
+ //猫头鹰右手(圆形的)
+ let rectRightHandGone = CGRect(x:mainSize.width / 2 + 62,
+ y:vLogin.frame.origin.y - 22, width:40, height:40)
+ imgRightHandGone = UIImageView(frame:rectRightHandGone)
+ imgRightHandGone.image = UIImage(named:"icon_hand")
+ self.view.addSubview(imgRightHandGone)
+ }
+ 
+ //输入框获取焦点开始编辑
+ func textFieldDidBeginEditing(textField:UITextField)
+ {
+ //如果当前是用户名输入
+ if textField.isEqual(txtUser){
+ if (showType != LoginShowType.PASS)
+ {
+ showType = LoginShowType.USER
+ return
+ }
+ showType = LoginShowType.USER
+ 
+ //播放不遮眼动画
+ UIView.animate(withDuration: 0.5, animations: { () -> Void in
+ self.imgLeftHand.frame = CGRect(
+ x:self.imgLeftHand.frame.origin.x - self.offsetLeftHand,
+ y:self.imgLeftHand.frame.origin.y + 30,
+ width:self.imgLeftHand.frame.size.width, height:self.imgLeftHand.frame.size.height)
+ self.imgRightHand.frame = CGRect(
+ x:self.imgRightHand.frame.origin.x + 48,
+ y:self.imgRightHand.frame.origin.y + 30,
+ width:self.imgRightHand.frame.size.width, height:self.imgRightHand.frame.size.height)
+ self.imgLeftHandGone.frame = CGRect(
+ x:self.imgLeftHandGone.frame.origin.x - 70,
+ y:self.imgLeftHandGone.frame.origin.y, width:40, height:40)
+ self.imgRightHandGone.frame = CGRect(
+ x:self.imgRightHandGone.frame.origin.x + 30,
+ y:self.imgRightHandGone.frame.origin.y, width:40, height:40)
+ })
+ }
+ //如果当前是密码名输入
+ else if textField.isEqual(txtPwd){
+ if (showType == LoginShowType.PASS)
+ {
+ showType = LoginShowType.PASS
+ return
+ }
+ showType = LoginShowType.PASS
+ 
+ //播放遮眼动画
+ UIView.animate(withDuration: 0.5, animations: { () -> Void in
+ self.imgLeftHand.frame = CGRect(
+ x:self.imgLeftHand.frame.origin.x + self.offsetLeftHand,
+ y:self.imgLeftHand.frame.origin.y - 30,
+ width:self.imgLeftHand.frame.size.width, height:self.imgLeftHand.frame.size.height)
+ self.imgRightHand.frame = CGRect(
+ x:self.imgRightHand.frame.origin.x - 48,
+ y:self.imgRightHand.frame.origin.y - 30,
+ width:self.imgRightHand.frame.size.width, height:self.imgRightHand.frame.size.height)
+ self.imgLeftHandGone.frame = CGRect(
+ x:self.imgLeftHandGone.frame.origin.x + 70,
+ y:self.imgLeftHandGone.frame.origin.y, width:0, height:0)
+ self.imgRightHandGone.frame = CGRect(
+ x:self.imgRightHandGone.frame.origin.x - 30,
+ y:self.imgRightHandGone.frame.origin.y, width:0, height:0)
+ })
+ }
+ }
+ 
+ override func didReceiveMemoryWarning() {
+ super.didReceiveMemoryWarning()
+ }
+ }
+ 
+ //登录框状态枚举
+ enum LoginShowType {
+ case NONE
+ case USER
+ case PASS
+ }*/
 
 
 
