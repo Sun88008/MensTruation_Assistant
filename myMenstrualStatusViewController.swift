c1f64471c48462adb1b54dfc7fa0ff3e0e318f55
Sun88008 <812009938@qq.com>
2019-04-01 16:51:48 +0800

    Update myMenstrualStatusViewController.swift

--- myMenstrualStatusViewController.swift
+++ myMenstrualStatusViewController.swift
@@ -23,7 +23,8 @@ class myMenstrualStatusViewController: UIViewController,UIPickerViewDelegate,UIP
     
     var pickView1 = UIPickerView()
     var pickView2 = UIPickerView()
-    var pickerDateToolbar = UIToolbar()
+    var pickerDateToolbar1 = UIToolbar()
+    var pickerDateToolbar2 = UIToolbar()
     let Date1 = NSArray(objects:"15天","16天","17天","18天","19天","20天","21天","22天","23天","24天","25天","26天","27天","28天","29天","30天","31天","32天","33天","34天","35天","36天","37天","38天","39天","40天","41天","42天","43天","44天","45天","46天","47天","48天","49天","50天")
     let Date2 = NSArray(objects:"1天","2天","3天","4天","5天","6天","7天","8天","9天","10天","11天","12天","13天","14天","15天")
     var btnOK = UIButton()
@@ -53,29 +54,44 @@ class myMenstrualStatusViewController: UIViewController,UIPickerViewDelegate,UIP
         
         //let actionSheet = UIActionSheet.init(title: "", delegate: self as! UIActionSheetDelegate, cancelButtonTitle: nil, destructiveButtonTitle: nil)
         
-        pickerDateToolbar = UIToolbar.init(frame: CGRect(x: 0,y: 0,width: 320, height: 44))
-        pickerDateToolbar.barStyle = UIBarStyle.blackOpaque
-        pickerDateToolbar.sizeToFit()
+        pickerDateToolbar1 = UIToolbar.init(frame: CGRect(x: 0,y: 0,width: 320, height: 44))
+        pickerDateToolbar1.barStyle = UIBarStyle.blackOpaque
+        pickerDateToolbar1.sizeToFit()
         
-        let barItems = NSMutableArray.init()
+        pickerDateToolbar2 = UIToolbar.init(frame: CGRect(x: 0,y: 0,width: 320, height: 44))
+        pickerDateToolbar2.barStyle = UIBarStyle.blackOpaque
+        pickerDateToolbar2.sizeToFit()
+        
+        let barItems1 = NSMutableArray.init()
+        let barItems2 = NSMutableArray.init()
         
         let cancelBtn = UIBarButtonItem.init(title: "取消", style: UIBarButtonItemStyle.plain, target: self, action: #selector(toolBarCancelClick))
-        barItems.add(cancelBtn)
+        barItems1.add(cancelBtn)
+        barItems2.add(cancelBtn)
         
         let flexSpace = UIBarButtonItem.init(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
-        barItems.add(flexSpace)
+        barItems1.add(flexSpace)
+        barItems2.add(flexSpace)
         
-        let doneBtn = UIBarButtonItem.init(title: "完成", style: UIBarButtonItemStyle.done, target: self, action: #selector(toolBarDoneClick))
-        barItems.add(doneBtn)
+        let doneBtn1 = UIBarButtonItem.init(title: "完成", style: UIBarButtonItemStyle.done, target: self, action: #selector(toolBarDoneClick1))
+        barItems1.add(doneBtn1)
+        
+        let doneBtn2 = UIBarButtonItem.init(title: "完成", style: UIBarButtonItemStyle.done, target: self, action: #selector(toolBarDoneClick2))
+        barItems2.add(doneBtn2)
 
-        pickerDateToolbar.setItems(barItems as? [UIBarButtonItem], animated: true)
-        pickerDateToolbar.tintColor = UIColor.black
-        pickerDateToolbar.barTintColor = UIColor.white
-
+        pickerDateToolbar1.setItems(barItems1 as? [UIBarButtonItem], animated: true)
+        pickerDateToolbar1.tintColor = UIColor.black
+        pickerDateToolbar1.barTintColor = UIColor.white
+        
+        pickerDateToolbar2.setItems(barItems2 as? [UIBarButtonItem], animated: true)
+        pickerDateToolbar2.tintColor = UIColor.black
+        pickerDateToolbar2.barTintColor = UIColor.white
+        
         // 重点的一句
         SetTime1.inputView = pickView1
-        self.SetTime1.inputAccessoryView = pickerDateToolbar
-        //SetTime2.inputView = pickView2
+        self.SetTime1.inputAccessoryView = pickerDateToolbar1
+        SetTime2.inputView = pickView2
+        self.SetTime2.inputAccessoryView = pickerDateToolbar2
         
         // Do any additional setup after loading the view.
     }
@@ -106,10 +122,16 @@ class myMenstrualStatusViewController: UIViewController,UIPickerViewDelegate,UIP
         self.view.endEditing(true)
     }
     
-    @objc func toolBarDoneClick(){
+    @objc func toolBarDoneClick1(){
         let row = pickView1.selectedRow(inComponent: 0) //获取当前行
         let value = Date1.object(at: row) as! String //获取行内数据
         self.SetTime1.text = value
+        self.view.endEditing(true)
+    }
+    @objc func toolBarDoneClick2(){
+        let row = pickView2.selectedRow(inComponent: 0) //获取当前行
+        let value = Date2.object(at: row) as! String //获取行内数据
+        self.SetTime2.text = value
         self.view.endEditing(true)
     }
 
