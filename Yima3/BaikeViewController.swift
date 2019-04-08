b73881c8c0bb05388ac055028887b4dd954080b2
Sun88008 <812009938@qq.com>
2019-04-08 11:17:27 +0800

    网页加载

--- Yima3/BaikeViewController.swift
+++ Yima3/BaikeViewController.swift
@@ -12,14 +12,56 @@ import AVOSCloud
 import SnapKit
 import Alamofire
 
-class BaikeViewController: UIViewController {
-
+class BaikeViewController: UIViewController, UIWebViewDelegate, UITextFieldDelegate {
+    @IBOutlet weak var txtSearch: UITextField!
+    @IBOutlet weak var baikeWebview: UIWebView!
+    @IBOutlet weak var loading: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.view.backgroundColor = UIColor(red: 255/255, green: 192/255, blue: 203/255,alpha: 1)
+        //添加代理委托
+        txtSearch.delegate = self
+        baikeWebview.delegate = self
+        
+        txtSearch.leftView = UIImageView.init(image: #imageLiteral(resourceName: "sousuo"))
+        txtSearch.contentMode = UIViewContentMode.center
+        txtSearch.leftViewMode = UITextFieldViewMode.always
+        txtSearch.clearButtonMode = UITextFieldViewMode.always
         // Do any additional setup after loading the view.
     }
 
+    //加载请求的方法
+    func loadUrl(url: String, web: UIWebView) {
+        
+        //载入输入的请求
+        let aUrl = NSURL(string: ("https://" + url))
+        let urlRequest = NSURLRequest(url: aUrl! as URL)
+        web.loadRequest(urlRequest as URLRequest)
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        loadUrl(url: txtSearch.text!, web: baikeWebview)
+        //解除textField的第一响应者的注册资格，键盘消失；若没有这一步，键盘会一直留在屏幕内
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        //轮圈开始转动
+        loading.startAnimating()
+        //状态栏的网络请求轮圈开始转动
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    //网页加载结束
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        loading.stopAnimating()
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
